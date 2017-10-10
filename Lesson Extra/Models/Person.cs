36c7f1d8491ade2f4ba31adbbca595eac26bad87
Helena Olsson <helena.maarit.olsson@gmail.com>
2017-10-10 11:58:39 +0200

    fixing my last commit

---
+++ Lesson Extra/Models/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Extra.Models
+{
+    class Person
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Occupation { get; set; }
+        public int Age { get; set; }
+
+
+
+        public static List<Person> GenerateListOfPeople()
+        {
+
+            var people = new List<Person>();
+
+            people.Add(new Person { FirstName = "Eric", LastName = "Flemming", Occupation = "Developer", Age = 24 });
+            people.Add(new Person { FirstName = "Steve", LastName = "Smith", Occupation = "Manager", Age = 40 });
+            people.Add(new Person { FirstName = "Brendan", LastName = "Enrick", Occupation = "Developer", Age = 30 });
+            people.Add(new Person { FirstName = "Jane", LastName = "Doe", Occupation = "Accountant", Age = 34 });
+            people.Add(new Person { FirstName = "Beth", LastName = "Stevens", Occupation = "Accountant", Age = 40 });
+            people.Add(new Person { FirstName = "Samual", LastName = "Jackson", Occupation = "Chef", Age = 23 });
+            people.Add(new Person { FirstName = "Johnny", LastName = "Depp", Occupation = "Chef", Age = 25 });
+            people.Add(new Person { FirstName = "Greg", LastName = "Olsson", Occupation = "Developer", Age = 37 });
+            people.Add(new Person { FirstName = "Chet", LastName = "Manly", Occupation = "Accountant", Age = 30 });
+            people.Add(new Person { FirstName = "Randy", LastName = "Johnson", Occupation = "Developer", Age = 32 });
+            people.Add(new Person { FirstName = "Cindy", LastName = "Crawford", Occupation = "Developer", Age = 26 });
+            people.Add(new Person { FirstName = "Hugh", LastName = "Heffner", Occupation = "Accountant", Age = 91 });
+            people.Add(new Person { FirstName = "Larry", LastName = "Mannheim", Occupation = "Chef", Age = 24 });
+            people.Add(new Person { FirstName = "Karl", LastName = "Johan", Occupation = "King", Age = 60 });
+            people.Add(new Person { FirstName = "John", LastName = "Wick", Occupation = "Accountant", Age = 32 });
+            people.Add(new Person { FirstName = "Magnus", LastName = "Eriksson", Occupation = "Manager", Age = 36 });
+            people.Add(new Person { FirstName = "Tim", LastName = "Sicksson", Occupation = "Developer", Age = 35 });
+            people.Add(new Person { FirstName = "Lauren", LastName = "Sweet", Occupation = "Accountant", Age = 32 });
+            people.Add(new Person { FirstName = "Will", LastName = "Smith", Occupation = "Chef", Age = 40 });
+            people.Add(new Person { FirstName = "Jason", LastName = "Stathom", Occupation = "Developer", Age = 38 });
+            people.Add(new Person { FirstName = "Elizabeth", LastName = "Hurley", Occupation = "Manager", Age = 55 });
+
+
+            return people;
+
+        }
+    }
+}
+
---
+++ Lesson Extra/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson_Extra.Models;
+using System.IO;
+using System.Threading;
+
+namespace Lesson_Extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //Write to a textfile using the person list, name and age of accountants over 33
+
+            // var allAcountants = 
+
+            //create a file with a song or poet, write it out in console
+
+            //string textToSave = "";
+            //var p = Person.GenerateListOfPeople();
+
+            //var accountantsOver33 = p.Where(x => x.Occupation.ToLower() == "accountant" && x.Age > 33);
+
+            //foreach (var item in accountantsOver33)
+            //{
+            //    textToSave += item.FirstName + " " + item.LastName.PadRight(10) + "Age: " + item.Age + " " + item.Occupation + Environment.NewLine;
+            //}
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            docPath += @"\poem.txt";
+
+            //File.WriteAllText(docPath, textToSave);
+
+            //Console.WriteLine(docPath);
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(docPath))
+                {
+                    string poem = sr.ReadToEnd();
+                    foreach (char c in poem)
+                    {
+                        Console.Write(c);
+                        Thread.Sleep(80);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("You did something wrong..");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+    
+    }
+}
